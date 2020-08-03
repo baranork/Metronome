2550f77861c8ea30d38776e368a076d5d8c40289
Facundo <Facundo@Facus-MacBook-Pro.local>
2020-08-03 20:06:22 -0300

    Updates

---
+++ precache-manifest.228f4a6fca42b5b06025e210c920528f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c9cbd17562c70dd39e601279c6077c",
+    "url": "/Metronome/index.html"
+  },
+  {
+    "revision": "fd96f121d74a4380e308",
+    "url": "/Metronome/static/css/main.5683ea8d.chunk.css"
+  },
+  {
+    "revision": "427f42762413a191f1bf",
+    "url": "/Metronome/static/js/2.e22a46a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Metronome/static/js/2.e22a46a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd96f121d74a4380e308",
+    "url": "/Metronome/static/js/main.c8c2fd88.chunk.js"
+  },
+  {
+    "revision": "e3727ce9a7d92229081a",
+    "url": "/Metronome/static/js/runtime-main.1f1cb3bc.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/Metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/Metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
